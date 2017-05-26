408f638e1bbaa314bf08a6df6df98666e4e26433
wingillis <win.gillis@gmail.com>
2017-05-25 22:25:42 -0400

    added some thoughts on the analysis

--- cnmfe_info.md
+++ cnmfe_info.md
@@ -1,4 +1,6 @@
-# Important Information about how CNMF_E works
+# why so serious?
+
+## Important Information about how CNMF_E works
 
 Descriptions of the data formats and functions utilized by the package
 
@@ -13,3 +15,15 @@ Important vars (all subcomponents of the Source2D object):
 | C_raw | contains the raw fluorescence (df/f?) from the detected neuron |
 | Cn | some sort of correlation? |
 | S | theoretical number of spikes exhibited at any given point for the neuron |
+
+## extract cell traces
+
+Make sure the parameters used for detecting cells are conditioned well for the
+data you present to it.
+
+Parameters that need to be set properly:
+- `min_pnr`
+- `min_corr`
+- `min_pixel`
+
+Check these with `cnmfe_show_pnr`
--- grin_cell_analysis.m
+++ grin_cell_analysis.m
@@ -1,8 +1,17 @@
 
 % load cell data from your current dir
 cells = load_cells('inscopix');
+% n x m where n=cells; m=time
 
 % TODO: add optional code to show how the cells spike over time
+show_trace = false;
+if show_trace
+  plot(cells(1:15, :)')
+  figure();
+  imagesc(cells(1:15, :)')
+end
+
+% do some quality control on the cells. Only choose stuff with high snr and low variance?
 
 % based on visual inspection, any spiking threshold is 2 or above
 spikes = detect_spikes(cells);
@@ -14,3 +23,8 @@ spikes = detect_spikes(cells);
 extr = kinect_extract_findall;
 
 % manually decide which dataset corresponds to ds of interest
+extr = extr(1);
+
+%% go through different numbers of cells to see which ones explain the labels that
+% the model generates the best (figure out how to do this)
+rng(1); % set seed to predetermined value
