88e12474ccc6f26e8b2496196c1a0b2fb7af39e6
wingillis <win.gillis@gmail.com>
2017-05-28 15:18:04 -0400

    Added minimal support for cnmfe extracted data

--- load_cells.m
+++ load_cells.m
@@ -1,26 +1,30 @@
 function [cells]=load_cells(type)
   % load extracted traces into matlab
   % TODO: add support for path specification
-  if ~exist('cells', 'var')==1
-    % load cells
+  % load cells
+  if strcmp(type, 'inscopix')
     objects = length(dir('Obj*'));
-    cells = {};
-    if strcmp(type, 'inscopix')
-      for i=1:objects
-        % this is for when inscopix saves a shitload of object files for extracted traces
-         tmp = load(sprintf('Obj_%d/Obj_2 - IC trace %d.mat', i, i), 'Object')
-         cells{i} = tmp.Object.Data;
-      end
-      % make it into a matrix
-      cells = cell2mat(cells);
+    cells = cell(objects, 1);
+    for i=1:objects
+      % this is for when inscopix saves a shitload of object files for extracted traces
+       tmp = load(sprintf('Obj_%d/Obj_2 - IC trace %d.mat', i, i), 'Object');
+       cells{i} = tmp.Object.Data;
     end
+    % make it into a matrix
+    cells = cell2mat(cells);
+  end
 
-    if strcmp(type, 'csv')
+  if strcmp(type, 'csv')
 
+  end
+
+  if strcmp(type, 'cnmfe')
+    files = dir('*_results.mat');
+    if length(files) > 1
+      disp('multiple results files found, loading the first one:')
+      fprintf('\t%s\n', files(1).name);
     end
-
-    if strcmp(type, 'cnmfe')
-
-    end
+    f = files(1).name;
+    cells = load(f);
   end
 end
