905303e938d1a1b4deaf59f405f0afd3033c5c03
wingillis <win.gillis@gmail.com>
2017-05-30 00:02:18 -0400

    Added some boiler

--- compare_rps_and_cells.m
+++ compare_rps_and_cells.m
@@ -1,2 +1,19 @@
 % I copied the kinect_extract object from the _analysis/backup folder on Jeff's
-% orchestra group folder 
+% orchestra group folder
+
+% directory
+d = '/Volumes/Data2/k9_01212017_kinect';
+cd(d);
+
+% load it up
+load('../kinect_object.mat')
+
+extr = kinect_extract(3);
+
+clear kinect_extract;
+
+% get the random projections
+rps = extr.projections.rp(:, 1:80);
+
+% load the cells
+load('motionCorre-borderless-downsample2x_results.mat')
