039d7cca62497f60d90c8ea0ae8e66d612d28922
wingillis <win.gillis@gmail.com>
2017-05-30 00:11:22 -0400

    Added a hifiber class into the mix

--- compare_rps_and_cells.m
+++ compare_rps_and_cells.m
@@ -17,3 +17,17 @@ rps = extr.projections.rp(:, 1:80);
 
 % load the cells
 load('motionCorre-borderless-downsample2x_results.mat')
+
+extr.load_inscopix_timestamps();
+
+
+fprintf('Nan frames %d', sum(isnan(extr.timestamps.inscopix)))
+
+
+grin = hifiber(neuron.C_raw(1:10, :), extr.timestamps.inscopix);
+
+figure(1);
+subplot(2,1,1);
+imagesc(rps(1:2000, :)');
+subplot(2,1,2);
+plot(neuron.C_raw(1:10, 1:2000)');
