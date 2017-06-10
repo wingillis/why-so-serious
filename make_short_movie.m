966f54732b7691daf399207b39c9348e0623ead8
Winthrop Gillis <win.gillis@gmail.com>
2017-06-10 16:03:24 -0400

    Added convenience function for using a subset of the data

---
+++ make_short_movie.m
@@ -0,0 +1,19 @@
+function outfile=make_short_movie(filepath, nframes)
+  if nargin < 2
+    nframes = 6000;
+  end
+  mf = matfile(filepath);
+  [fpath, fname, ext] = fileparts(filepath);
+  outfile = matfile(fullfile(fpath, [fname '-short.mat']), 'Writable', true);
+  sizY = mf.sizY;
+  sizY(3) = nframes; % arbitrary # of frames for a smaller version
+  outfile.sizY = sizY;
+  outfile.Y = zeros(sizY(1), sizY(2), 2, 'uint16');
+  for i=1:nframes
+    tmp = mf.Y(:,:,i);
+    outfile.Y(:,:,i) = tmp;
+    if mod(i, 100) == 0
+      fprintf('Frame %d\n', i);
+    end
+  end
+end % function
