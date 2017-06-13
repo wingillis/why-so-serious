cec78aa1450f6b6e3d861b8eadb8c633de2b7929
wingillis <win.gillis@gmail.com>
2017-06-13 19:35:56 -0400

    Testing part2 function

--- 3part-cnmfe/part2_validation.m
+++ 3part-cnmfe/part2_validation.m
@@ -1,16 +1,11 @@
 function part2_validation(fname)
-%% load the neuron/data here
+  %% load the neuron/data here
 
+  load(fname);
 
-% look for file that has 'unprocessed' in its name
-dat_data = dir('*unprocessed*.mat');
-dat_data_file = dat_data(1).name;
-% load it into this workspace
-load(dat_data_file);
-
-%% delete, trim, split neurons
-neuron.viewNeurons([], neuron.C_raw);
-
-save(strrep(dat_data_file, 'unprocessed', '_processed'), '-v7.3');
+  %% delete, trim, split neurons
+  neuron.viewNeurons([], neuron.C_raw);
+  % assumes the file you gave it has _unprocessed in its name
+  save([strrep(fname, '_unprocessed.mat', '') '_processed.mat'], '-v7.3');
 
 end
