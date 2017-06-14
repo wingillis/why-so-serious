2f91740066e80e4b1762c172f0ffe4b028c986ef
wingillis <win.gillis@gmail.com>
2017-06-14 13:00:28 -0400

    Added a doc for this function

--- load_object_tree.m
+++ load_object_tree.m
@@ -1,12 +1,14 @@
 function [data]=load_object_tree(pth, type)
+  %% loads the desired object you want that was saved from mosaic (inscopix
+  % software). pth is the path to the dir the ica traces were saved
 
   switch type
-  case 'trace'
-    tree = construct_object_tree(pth, 'Obj_2*trace*.mat');
-  case 'mixing'
-    tree = construct_object_tree(pth, 'Obj_3*mixing*.mat');
-  case 'unmixing'
-    tree = construct_object_tree(pth, 'Obj_1*unmixing*.mat');
+    case 'trace'
+      tree = construct_object_tree(pth, 'Obj_2*trace*.mat');
+    case 'mixing'
+      tree = construct_object_tree(pth, 'Obj_3*mixing*.mat');
+    case 'unmixing'
+      tree = construct_object_tree(pth, 'Obj_1*unmixing*.mat');
   end
   data = cell(length(tree), 1);
   for i=1:length(tree)
