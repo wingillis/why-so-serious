c0a98d93044443e347b5ad74241f7f357cb49dbc
wingillis <win.gillis@gmail.com>
2017-06-15 14:01:09 -0400

    Added username for cluster config

--- 3part-cnmfe/instantiate_cluster.m
+++ 3part-cnmfe/instantiate_cluster.m
@@ -3,5 +3,6 @@ function [c]=instantiate_cluster()
   ClusterInfo.setQueueName('mpi');
   ClusterInfo.setWallTime('24:00:00');
   ClusterInfo.setMemUsage('20G');
+  ClusterInfo.setUserNameOnCluster('wg41');
   c = parcluster();
 end
