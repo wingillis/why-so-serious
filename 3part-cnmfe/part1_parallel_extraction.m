feac9a922fa8274e513350420c14a494f0a496d9
wingillis <win.gillis@gmail.com>
2017-06-15 13:20:32 -0400

    Concatenate the files cell array

--- 3part-cnmfe/part1_parallel_extraction.m
+++ 3part-cnmfe/part1_parallel_extraction.m
@@ -4,7 +4,7 @@ function [processed_path]=part1_parallel_extraction(nam, options)
   % by a factor of 50
   cnmfe_files = strsplit(genpath('/home/wg41/code/CNMF_E'), ':');
   grin_files = strsplit(genpath('/home/wg41/code/grin-analysis'), ':');
-  additional_files = {cnmfe_files grin_files};
+  additional_files = cat(2, cnmfe_files, grin_files);
 
   % instantiate a cluster
   c = instantiate_cluster();
