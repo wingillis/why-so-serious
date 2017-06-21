3ad168978b4e7767a5df7c955255d295dec319d3
wingillis <win.gillis@gmail.com>
2017-06-21 19:26:22 -0400

    Added some scripts for processing kinect-aligned data

---
+++ projection-cell-analysis/cp_length_characterization.m
@@ -0,0 +1,12 @@
+figure();
+thresholds = [0 0.1 0.2 0.25 0.29 0.33 0.4];
+for i=1:length(thresholds)
+  g.find_cp_peaks(thresholds(i));
+  [xi, f] = ksdensity(diff(g.changepoints));
+  plot(f, xi, 'LineWidth', 2);
+  hold on;
+end
+
+legend(cellfun(@(x) num2str(x), num2cell(thresholds), 'UniformOutput', false));
+xlabel('Frames btw changepoints')
+ylabel('Probability')
