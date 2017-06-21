947564ead959cb831a1fd3e858983050722f1eda
wingillis <win.gillis@gmail.com>
2017-06-21 19:31:09 -0400

    Added title to cp length script

--- projection-cell-analysis/cp_length_characterization.m
+++ projection-cell-analysis/cp_length_characterization.m
@@ -1,6 +1,7 @@
 figure();
 thresholds = [0 0.1 0.2 0.25 0.29 0.33 0.4];
 for i=1:length(thresholds)
+  % what am I doing here? I am using different thresholds for the changepoints
   g.find_cp_peaks(thresholds(i));
   [xi, f] = ksdensity(diff(g.changepoints));
   plot(f, xi, 'LineWidth', 2);
@@ -10,3 +11,4 @@ end
 legend(cellfun(@(x) num2str(x), num2cell(thresholds), 'UniformOutput', false));
 xlabel('Frames btw changepoints')
 ylabel('Probability')
+title('Comparison of cp peaks with different thresholds')
