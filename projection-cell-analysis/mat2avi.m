8225cc647cffb667bc8897fcb1f292cc3f181dc9
Winthrop Gillis <win.gillis@gmail.com>
2017-06-25 13:25:50 -0400

    add contours to raw movie

--- projection-cell-analysis/mat2avi.m
+++ projection-cell-analysis/mat2avi.m
@@ -1,21 +1,27 @@
 function mat2avi(fpath, outputname)
+  % read in the whole movie and calculate some mins and maxes
   [basename, ~, ~] = fileparts(fpath);
+  load(fpath);
   mf = matfile(fpath);
   siz = mf.sizY;
   % make 5 min movies
   fivemin = 30*5*60;
+  maxpx = 0;
+  minpx = 0;
   for i=1:floor(siz(3)/fivemin)
     w = VideoWriter(fullfile(basename, sprintf('%s-%d.avi', outputname, i)));
     open(w);
-    for j=1:fivemin
-      % TODO: make this scale less arbitrary
-      frame = single(mf.Y(:,:,(i-1)*fivemin + j));
-      frame = frame - 183;
-      frame(frame < 0) = 0;
-      frame = frame ./ 870;
-      frame(frame > 1) = 1;
-      writeVideo(w, frame);
+    y = single(mf.Y(:,:, (i-1)*fivemin + 1:i*fivemin));
+    if maxpx == 0
+      maxpx = max(y(:));
+      minpx = min(y(:));
     end
+    y = y - minpx;
+    y(y < 0) = 0;
+    y = y ./ maxpx;
+    y(y > 1) = 1;
+    y = permute(y, [1 2 4 3]);
+    writeVideo(w, y);
     fprintf('Video %d done\n', i);
     close(w);
   end
