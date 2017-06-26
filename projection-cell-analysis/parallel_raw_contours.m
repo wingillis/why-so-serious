580d66ed4070cba0650274a65402e29f5351de78
wingillis <win.gillis@gmail.com>
2017-06-26 13:06:22 -0400

    how to draw onto matrices

--- projection-cell-analysis/parallel_raw_contours.m
+++ projection-cell-analysis/parallel_raw_contours.m
@@ -1,4 +1,9 @@
 function parallel_raw_contours(fname, cnmfefname)
   % read in the size of the movie and send indices to each
   % batch script to make 1 min movies
+  c = parcluster();
+  mf = matfile(fname);
+
+  %insertShape(mtx, 'Polygon', coords, 'LineWidth', 1);
+  
 end
