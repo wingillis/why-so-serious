18be8bebec1f37898eced9800071ee00c7afc609
wingillis <win.gillis@gmail.com>
2017-06-29 15:43:59 -0400

    Better param scans

--- cnmfe-characterization/merging_neurons.m
+++ cnmfe-characterization/merging_neurons.m
@@ -1,28 +1,25 @@
 function [neuron_count]=merging_neurons(neuron)
   display_merge = false;
   view_neurons = false;
-  npoints = 30;
+  npoints = 25;
   neuron_win = neuron.copy();
 
-  neuron_count = zeros(npoints, npoints, npoints);
+  neuron_count = zeros(npoints, npoints);
 
   % which correlation thresholds to change
   spatial = linspace(0.3, 0, npoints);
   temporal = linspace(0.3, 0, npoints);
   spiking = linspace(0.1, 0, npoints);
 
-  merge_thr = [1, 1, 0.1];
+  merge_thr = [1, 1, 0.04];
 
   for i=1:npoints
     merge_thr(1) = spatial(i);
+    neuron = neuron_win.copy();
     for j=1:npoints
       merge_thr(2) = temporal(j);
-      neuron = neuron_win.copy();
-      for k=1:npoints
-        merge_thr(3) = spiking(k);
-        cnmfe_quick_merge;
-        neuron_count(i, j, k) = size(neuron.C, 1);
-      end
+      cnmfe_quick_merge;
+      neuron_count(i, j) = size(neuron.C, 1);
     end
   end
 end % function
