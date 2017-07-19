be5b2399c2b61757ec6624a443b0b3960df32c8d
wingillis <win.gillis@gmail.com>
2017-07-19 19:17:04 -0400

    graphing the thesholds now

--- 3part-cnmfe/merging_gridsearch.m
+++ 3part-cnmfe/merging_gridsearch.m
@@ -32,10 +32,13 @@ function merging_gridsearch(fname, opt)
   % construct the parameters to search: correlation thresholds
   % 0.8 cutoff is slightly arbitrary, but in the past I havent
   % seen much change with numbers higher than this
-  npoints = 10;
-  spatial = linspace(0.8, 0, npoints);
-  temporal = linspace(0.8, 0, npoints);
-  spike_thresh = 0.1;
+  npoints = 40;
+  maxthresh = 0.8;
+  spatial = linspace(maxthresh, 0, npoints);
+  temporal = linspace(maxthresh, 0, npoints);
+  spike_thresh = 0.05;
+
+  % TODO: add spike_thresh as a searchable parameter
 
   [S, T] = ndgrid(spatial, temporal);
 
@@ -70,5 +73,31 @@ function merging_gridsearch(fname, opt)
   save('neuron_count.mat', 'neuron_count');
 
   delete(job);
+
+  nc = cell2mat(neuron_count);
+
+  nc = reshape(nc, npoints, npoints);
+
+  figure();
+  [xx, yy] = meshgrid(1:npoints, 1:npoints);
+
+  surf(xx, yy, nc);
+  colormap bone;
+
+  xticks(linspace(1, npoints, 10));
+  xticklabel(linspace(maxthresh, 0, 10));
+  yticks(linspace(1, npoints, 10));
+  yticklabel(linspace(maxthresh, 0, 10));
+
+  view(0, 90);
+  print('grid1', '-dpng', '-r300');
+
+  view(135, 60);
+  print('grid2', '-dpng', '-r300');
+
+  view(-45, 60);
+  print('grid3', '-dpng', '-r300');
+
+  savefig(gcf, 'grid');
 
 end % function
