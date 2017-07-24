2172b16c79803841d660e531f1b713053d5a2d55
wingillis <win.gillis@gmail.com>
2017-07-24 13:13:12 -0400

    some comments

--- projection-cell-analysis/grin_movie_stats.m
+++ projection-cell-analysis/grin_movie_stats.m
@@ -1,11 +1,14 @@
 function grin_movie_stats(matname, outfile)
+  % produces 3 images based on the maximum projection,
+  % the mean projection,
+  % and the max - mean / mean (almost dff)
   [basename, ~, ~] = fileparts(matname);
 
   mf = matfile(matname);
   siz = mf.sizY;
   meanframe = zeros(siz(1), siz(2), 'double');
   maxframe = zeros(size(meanframe), 'double');
-  % read in many frames ()
+  % read in chunks of frames
   fivemin = 30 * 60 * 5;
   chunks = floor(siz(3)/fivemin);
   for i=1:chunks
@@ -18,6 +21,7 @@ function grin_movie_stats(matname, outfile)
   end
 
   img = (double(maxframe) - meanframe) ./ meanframe;
+  % returns the percentile range of values
   ptile = @(x) [prctile(x(:), 0.5) prctile(x(:), 99.5)];
 
   imwrite(mat2gray(img, ptile(img)), fullfile(basename, [outfile '-dff.png']), 'png');
