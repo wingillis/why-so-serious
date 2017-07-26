3863a7881bd2ef1cd74806e709993d3814be38d6
wingillis <win.gillis@gmail.com>
2017-07-26 10:46:43 -0400

    added dff function to use btw figures

--- figure-code/roi_stackplot.m
+++ figure-code/roi_stackplot.m
@@ -1,15 +1,34 @@
-function roi_stackplot(cnmfe_file)
-  load(cnmfe_file);
-
+function roi_stackplot(cnmfe_file, movie_file)
   num_plots = 10;
+  colors = cubehelix(num_plots, 0.0, -0.34, 0.96, 0.83, [0.13 0.90], [0.19 0.85]);
+  load(cnmfe_file);
+  mf = matfile(movie_file);
+  img = dff(mf, 1000);
   figure();
+  subplot(2, 1, 1);
+  imshow(1-repmat(img, 1, 1, 3));
+  hold on;
+  for i=1:num_plots
+    h = imshow(permute(repmat(colors(i, :), d1, 1, d2), [1 3 2]));
+    set(h, 'AlphaData', reshape(neuron.A(:,i), d1, d2));
+  end
+  hold off;
 
-  % plot only the first ten neurons
-  data = neuron.C(1:num_plots, :);
-  data = zscore(data, [], 2);
+  subplot(2,1,2);
+  % TODO: decide btw C and C_raw
+  data = neuron.C_raw(1:num_plots, :);
+  maxes = max(data, [], 2);
   additions = 1:num_plots;
-  data = data * additions';
+  for i=additions
+    data(i, :) = data(i, :) ./ maxes(i);
+    data(i, :) = data(i, :) + i;
+  end
 
-  plot(data);
+  colormap(colors);
+  plot(data');
+  box off;
+  axis tight;
+
+  % TODO: cubehelix colorsceme
 
 end % function
