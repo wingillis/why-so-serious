c24c7ab083684304cb98d4772bc16f4cd6983a60
wingillis <win.gillis@gmail.com>
2017-07-26 10:55:14 -0400

    save file now too

--- figure-code/roi_stackplot.m
+++ figure-code/roi_stackplot.m
@@ -1,10 +1,10 @@
-function roi_stackplot(cnmfe_file, movie_file)
+function roi_stackplot(cnmfe_file, movie_file, savefolder)
   num_plots = 10;
   colors = cubehelix(num_plots, 0.0, -0.34, 0.96, 0.83, [0.13 0.90], [0.19 0.85]);
   load(cnmfe_file);
   mf = matfile(movie_file);
   img = dff(mf, 1000);
-  figure();
+  f = figure();
   subplot(2, 1, 1);
   imshow(repmat(img, 1, 1, 3));
   hold on;
@@ -19,16 +19,16 @@ function roi_stackplot(cnmfe_file, movie_file)
   data = neuron.C_raw(1:num_plots, :);
   maxes = max(data, [], 2);
   additions = 1:num_plots;
+  hold on;
   for i=additions
     data(i, :) = data(i, :) ./ maxes(i);
     data(i, :) = data(i, :) + i;
+    plot(data(i,:), 'color', colors(i,:));
   end
-
-  colormap(colors);
-  plot(data');
   box off;
   axis tight;
 
-  % TODO: cubehelix colorsceme
+  saveas(f, fullfile(savefolder, 'roi_stackplot'), 'epsc');
+  print(f, fullfile(savefolder, 'roi_stackplot'), '-dpng', '-r300');
 
 end % function
