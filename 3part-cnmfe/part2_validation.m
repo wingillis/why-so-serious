d2de31b006a0936b09f81f0fbd539758367c34a6
wingillis <win.gillis@gmail.com>
2017-07-27 12:45:47 -0400

    Changed to displayNeurons

--- 3part-cnmfe/part2_validation.m
+++ 3part-cnmfe/part2_validation.m
@@ -4,7 +4,8 @@ function part2_validation(fname)
   load(fname);
 
   %% delete, trim, split neurons
-  neuron.viewNeurons([], neuron.C_raw);
+  % neuron.viewNeurons([], neuron.C_raw);
+  neuron.displayNeurons([], neuron.C_raw);
   % assumes the file you gave it has _unprocessed in its name
   fname = [strrep(fname, '_unprocessed.mat', '') '_processed.mat'];
   save(fname, 'neuron', 'd1', 'd2', 'numFrame', 'options', 'Fs', '-v7.3')
