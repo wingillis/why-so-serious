20213376798c037c34b29765c784a488afec49ad
wingillis <win.gillis@gmail.com>
2017-07-27 17:28:48 -0400

    added more values and display as a proportion

--- figure-code/overlapping_cell_counts.m
+++ figure-code/overlapping_cell_counts.m
@@ -1,15 +1,22 @@
 function overlapping_cell_counts()
   values = [1 8 14;...
             1 25 24;...
-            1 16 25];
+            1 16 25;...
+            18 223 309;...
+            24 213 263];
+  totals = sum(values, 2);
+  for i=1:size(values, 1)
+    values(i,:) = values(i,:) ./ totals(i);
+  end
   f = figure();
-  bar(1, mean(a(:,1)), 'facecolor', [222 188 24]./255)
-  bar(2, mean(a(:,2)), 'facecolor', [1 0 0])
-  bar(3, mean(a(:,3)), 'facecolor', [0 1 0])
-  errorbar(1:3, mean(a, 1), std(a, [], 1), 'k.')
+  hold on;
+  bar(1, mean(values(:,1)), 'facecolor', [222 188 24]./255);
+  bar(2, mean(values(:,2)), 'facecolor', [1 0 0]);
+  bar(3, mean(values(:,3)), 'facecolor', [0 1 0]);
+  errorbar(1:3, mean(values, 1), std(values, [], 1), 'k.')
   xticks(1:3)
   xticklabels({'both', 'red', 'green'});
-  ylabel('# cells');
+  ylabel('proportion');
   f.PaperUnits = 'inches';
   f.PaperPosition = [0 0 4 3];
   saveas(f, '/Users/wgillis/Dropbox (HMS)/lab-datta/projects/dls-function/paper/figs/overlapping-cell-count', 'epsc');
