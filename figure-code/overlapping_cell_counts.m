d6485d00e3522673e1c6def38f9a0ca8150adea9
wingillis <win.gillis@gmail.com>
2017-07-29 20:11:21 -0400

    smaller overlap fig, plot injection coords

--- figure-code/overlapping_cell_counts.m
+++ figure-code/overlapping_cell_counts.m
@@ -15,10 +15,11 @@ function overlapping_cell_counts()
   bar(3, mean(values(:,3)), 'facecolor', [0 1 0]);
   errorbar(1:3, mean(values, 1), std(values, [], 1), 'k.')
   xticks(1:3)
-  xticklabels({'both', 'd1', 'd2'});
+  xticklabels({'both (n=45)', 'd1 (n=485)', 'd2 (n=635)'});
+  set(gca, 'fontsize', 4);
   ylabel('proportion per sample');
   f.PaperUnits = 'inches';
-  f.PaperPosition = [0 0 2 2.5];
+  f.PaperPosition = [0 0 1.5 2];
   saveas(f, '/Users/wgillis/Dropbox (HMS)/lab-datta/projects/dls-function/paper/figs/cre-on-off overlap/overlapping-cell-count', 'epsc');
   savefig(f, '/Users/wgillis/Dropbox (HMS)/lab-datta/projects/dls-function/paper/figs/cre-on-off overlap/overlapping-cell-count');
   print(f, '/Users/wgillis/Dropbox (HMS)/lab-datta/projects/dls-function/paper/figs/cre-on-off overlap/overlapping-cell-count', '-r300', '-dtiff');
