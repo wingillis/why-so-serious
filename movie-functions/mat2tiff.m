a05e5c960acb1ff00b0dfefd5d50899800944615
wingillis <win.gillis@gmail.com>
2017-12-11 20:54:48 -0500

    Added some verbiage

--- movie-functions/mat2tiff.m
+++ movie-functions/mat2tiff.m
@@ -3,9 +3,12 @@ function mat2tiff(fname)
   % that I've already made and pass them through the inscopix processing
   % pipeline
   tifname = [fname(1:end-3) 'tif'];
+  disp(sprintf('Output file name: %s', tifname));
   mf = matfile(fname);
+  disp('Loading in the data...');
   data = mf.Y(:,:,:);
   frames = mf.sizY(1,3);
+  disp('Loaded - writing new tif files...');
   imwrite(mf.Y(:,:,1), tifname);
   for i=2:frames
     if mod(i, 500) == 0
