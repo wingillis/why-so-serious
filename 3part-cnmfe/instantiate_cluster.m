19ad183780c783e662e58be60fa9e000409a5c70
Winthrop Gillis <win.gillis@gmail.com>
2017-12-12 14:40:48 -0500

    changed for 2017b

--- 3part-cnmfe/instantiate_cluster.m
+++ 3part-cnmfe/instantiate_cluster.m
@@ -4,14 +4,16 @@ function [c]=instantiate_cluster(options)
     configCluster();
   end
   % condition for our queue - configure thru your config file
-  ClusterInfo.setQueueName(options.cluster.queue);
-  ClusterInfo.setWallTime(options.cluster.wall_time);
-  ClusterInfo.setMemUsage(options.cluster.mem);
-  ClusterInfo.setUserNameOnCluster(options.cluster.username);
-  ClusterInfo.setEmailAddress(options.cluster.email);
-  ClusterInfo.setUserDefinedOptions(options.cluster.user_defined_opts);
+  % ClusterInfo.setQueueName(options.cluster.queue);
+  % ClusterInfo.setWallTime(options.cluster.wall_time);
+  % ClusterInfo.setMemUsage(options.cluster.mem);
+  % ClusterInfo.setUserNameOnCluster(options.cluster.username);
+  % ClusterInfo.setEmailAddress(options.cluster.email);
+  % ClusterInfo.setUserDefinedOptions(options.cluster.user_defined_opts);
   % create the cluster object
-  c = parcluster();
+  c = parcluster('o2 local R2017b');
   c.AdditionalProperties.WallTime = options.cluster.wall_time;
   c.AdditionalProperties.QueueName = options.cluster.queue;
+  c.AdditionalProperties.MemUsage = options.cluster.mem;
+  c.saveProfile;
 end
