b43496bc8f8b818fd5939b98b0d102f37fe91da1
wingillis <win.gillis@gmail.com>
2017-12-11 20:43:21 -0500

    added function to convert downsampled mat files to tif files

---
+++ movie-functions/mat2tiff.m
@@ -0,0 +1,16 @@
+function mat2tiff(fname)
+  %% this function is mainly to use the downsampled recording files
+  % that I've already made and pass them through the inscopix processing
+  % pipeline
+  tifname = [fname(1:end-3) 'tif'];
+  mf = matfile(fname);
+  frames = mf.sizY(1,3);
+  imwrite(mf.Y(:,:,1), tifname);
+  for i=2:frames
+    if mod(i, 500)
+      fprintf('Wrote %d frames out of %d\n', i, frames);
+    end
+    imwrite(mf.Y(:,:,i), tifname, 'WriteMode', 'append');
+  end
+
+end % function
