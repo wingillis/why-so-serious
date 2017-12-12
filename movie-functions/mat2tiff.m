b96e3637b5c338a7dde505a8ac07bd22369dc650
wingillis <win.gillis@gmail.com>
2017-12-11 21:23:43 -0500

    try..catch block for when the tif file gets too large

--- movie-functions/mat2tiff.m
+++ movie-functions/mat2tiff.m
@@ -2,7 +2,8 @@ function mat2tiff(fname)
   %% this function is mainly to use the downsampled recording files
   % that I've already made and pass them through the inscopix processing
   % pipeline
-  tifname = [fname(1:end-3) 'tif'];
+  prepend = 1;
+  tifname = sprintf('%s-%03d.tif', fname(1:end-4), prepend);
   disp(sprintf('Output file name: %s', tifname));
   mf = matfile(fname);
   disp('Loading in the data...');
@@ -11,10 +12,18 @@ function mat2tiff(fname)
   disp('Loaded - writing new tif files...');
   imwrite(mf.Y(:,:,1), tifname);
   for i=2:frames
-    if mod(i, 500) == 0
-      fprintf('Wrote %d frames out of %d\n', i, frames);
+    try
+      if mod(i, 500) == 0
+        fprintf('Wrote %d frames out of %d\n', i, frames);
+      end
+      imwrite(data(:,:,i), tifname, 'WriteMode', 'append');
+    catch
+      disp('File too big - writing new file now');
+      prepend = prepend + 1;
+      tifname = sprintf('%s-%03d.tif', fname(1:end-4), prepend);
+      imwrite(data(:,:,i), tifname);
     end
-    imwrite(data(:,:,i), tifname, 'WriteMode', 'append');
+
   end
 
 end % function
