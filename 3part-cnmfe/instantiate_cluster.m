7b96bc12f0b5e20410be5d18e991e9cf0b7c941f
Winthrop Gillis <win.gillis@gmail.com>
2017-12-12 22:22:53 -0500

    getting ready for motion correction

--- 3part-cnmfe/instantiate_cluster.m
+++ 3part-cnmfe/instantiate_cluster.m
@@ -1,19 +1,15 @@
 function [c]=instantiate_cluster(options)
   % needed to run things through O2
-  if ~any(strcmp(parallel.clusterProfiles, 'o2 local R2017b'))
+  if ~any(strcmp(parallel.clusterProfiles, 'o2 local R2016b'))
     configCluster();
   end
   % condition for our queue - configure thru your config file
-  % ClusterInfo.setQueueName(options.cluster.queue);
-  % ClusterInfo.setWallTime(options.cluster.wall_time);
-  % ClusterInfo.setMemUsage(options.cluster.mem);
-  % ClusterInfo.setUserNameOnCluster(options.cluster.username);
-  % ClusterInfo.setEmailAddress(options.cluster.email);
-  % ClusterInfo.setUserDefinedOptions(options.cluster.user_defined_opts);
+  ClusterInfo.setQueueName(options.cluster.queue);
+  ClusterInfo.setWallTime(options.cluster.wall_time);
+  ClusterInfo.setMemUsage(options.cluster.mem);
+  ClusterInfo.setUserNameOnCluster(options.cluster.username);
+  ClusterInfo.setEmailAddress(options.cluster.email);
+  ClusterInfo.setUserDefinedOptions(options.cluster.user_defined_opts);
   % create the cluster object
-  c = parcluster('o2 local R2017b');
-  c.AdditionalProperties.WallTime = options.cluster.wall_time;
-  c.AdditionalProperties.QueueName = options.cluster.queue;
-  c.AdditionalProperties.MemUsage = options.cluster.mem;
-  c.saveProfile;
+  c = parcluster();
 end
