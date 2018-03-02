c082e4bd4c89face3915764bfcc6b6f616237e34
Winthrop Gillis <win.gillis@gmail.com>
2018-03-02 18:45:02 -0500

    added a script to compute a subsampled background from inscopix recordings

--- reconstruct_background.m
+++ reconstruct_background.m
@@ -7,13 +7,13 @@ mf = matfile(['background-' fname], 'writable', true);
 sz = 0;
 
 for i=1:numel(neuron.batches)
-	bneuron = neuron.batches{i};
-	ybg = bneuron.reconstruct_background();
-	sz = sz + size(ybg, 3);
+	fprintf('Running on batch %d\n', i);
+	bneuron = neuron.batches{i}.neuron;
+	ybg = bneuron.reconstruct_background([1 size(bneuron.C_prev, 2)]);
 	if i == 1
 		mf.ybg = ybg;
 	else
-		mf.ybg(:, :, sz+1:size(ybg, 3) + sz +1) = ybg;
+		mf.ybg(:, :, sz+1:size(ybg, 3) + sz) = ybg;
 	end
-
+	sz = sz + size(ybg, 3);
 end
---
+++ subsample_background.m
@@ -0,0 +1,30 @@
+function subsample_background(fname)
+	mf = matfile(fname);
+	sz = size(mf, 'ybg');
+	numsamples = 2000;
+	iterations = floor(sz(3)/numsamples);
+	subs = 5;
+	xs = floor(sz(2)/subs);
+	ys = floor(sz(1)/subs);
+	[xx, yy] = meshgrid(1:xs:sz(2), 1:ys:sz(1));
+	xx = xx(:);
+	yy = yy(:);
+	traces = zeros(subs^2, 1);
+	for i=1:iterations+1
+		start = (i-1)*numsamples + 1;
+		en = i*numsamples;
+		if en>sz(3)
+			e = sz(3);
+		end
+		y = mf.ybg(:,:,start:en);
+		fprintf('Iteration %d\n', i);
+		for j=1:numel(xx)
+			fprintf('Chunk %d\n', j);
+			chunk = y(yy(j):yy(j)+ys-1, xx(j):xx(j)+xs-1, :);
+			chunk = reshape(chunk, xs*ys, []);
+			chunk = mean(chunk, 1);
+			traces(j, start:en) = chunk;
+		end
+	end
+	save(['subsample-' fname], 'traces');
+end
